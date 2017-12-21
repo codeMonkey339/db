eb537b6760fa06246e98519b4c60a5a1b9c48262
Nov11 <529079634@qq.com>
2017-12-21 11:04:39 +0800

    task2 is done. testing passed.

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -6,34 +6,68 @@
 
 namespace cmudb {
 
-template <typename T> LRUReplacer<T>::LRUReplacer() {}
+template<typename T>
+LRUReplacer<T>::LRUReplacer() {}
 
-template <typename T> LRUReplacer<T>::~LRUReplacer() {}
+template<typename T>
+LRUReplacer<T>::~LRUReplacer() {}
 
 /*
  * Insert value into LRU
  */
-template <typename T> void LRUReplacer<T>::Insert(const T &value) {}
+template<typename T>
+void LRUReplacer<T>::Insert(const T &value) {
+  std::lock_guard<std::mutex> guard(mtx);
+  if (hash.find(value) != hash.end()) {
+    auto iter = hash[value];
+    lst.erase(iter);
+  }
+
+  lst.insert(lst.begin(), value);
+  hash[value] = lst.begin();
+}
 
 /* If LRU is non-empty, pop the head member from LRU to argument "value", and
  * return true. If LRU is empty, return false
  */
-template <typename T> bool LRUReplacer<T>::Victim(T &value) {
-  return false;
+template<typename T>
+bool LRUReplacer<T>::Victim(T &value) {
+  std::lock_guard<std::mutex> guard(mtx);
+  if (lst.empty()) {
+    return false;
+  }
+  value = *lst.rbegin();
+  lst.pop_back();
+  hash.erase(value);
+  return true;
 }
 
 /*
  * Remove value from LRU. If removal is successful, return true, otherwise
  * return false
  */
-template <typename T> bool LRUReplacer<T>::Erase(const T &value) {
-  return false;
+template<typename T>
+bool LRUReplacer<T>::Erase(const T &value) {
+  std::lock_guard<std::mutex> guard(mtx);
+  if (hash.find(value) == hash.end()) {
+    return false;
+  }
+  auto iter = hash[value];
+  lst.erase(iter);
+  hash.erase(value);
+  return true;
 }
 
-template <typename T> size_t LRUReplacer<T>::Size() { return 0; }
+template<typename T>
+size_t LRUReplacer<T>::Size() {
+  std::lock_guard<std::mutex> guard(mtx);
+  return hash.size();
+}
 
-template class LRUReplacer<Page *>;
+template
+class LRUReplacer<Page *>;
 // test only
-template class LRUReplacer<int>;
+template
+class LRUReplacer<int>;
 
 } // namespace cmudb
