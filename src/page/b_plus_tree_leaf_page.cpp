a0b177e2d2c9f89d806e87bc5d3041554e9937f0
Nov11 <529079634@qq.com>
2017-12-23 19:59:09 +0800

    working on project2 task1

--- src/page/b_plus_tree_leaf_page.cpp
+++ src/page/b_plus_tree_leaf_page.cpp
@@ -20,18 +20,25 @@ namespace cmudb {
  * next page id and set max size
  */
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id) {
+  SetPageType(IndexPageType::LEAF_PAGE);
+  SetSize(0);
+  SetPageId(page_id);
+  SetParentPageId(parent_id);
+  SetNextPageId(INVALID_PAGE_ID);
+  SetMaxSize((PAGE_SIZE - sizeof(BPlusTreePage)) / sizeof(MappingType));
+}
 
 /**
  * Helper methods to set/get next page id
  */
 INDEX_TEMPLATE_ARGUMENTS
 page_id_t B_PLUS_TREE_LEAF_PAGE_TYPE::GetNextPageId() const {
-  return INVALID_PAGE_ID;
+  return next_page_id_;
 }
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) { next_page_id_ = next_page_id; }
 
 /**
  * Helper method to find the first index i so that array[i].first >= key
@@ -40,7 +47,18 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
 INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::KeyIndex(
     const KeyType &key, const KeyComparator &comparator) const {
-  return 0;
+  int len = GetSize();
+  int b = 0;
+  int e = len;
+  while (b < e) {
+    int mid = b + (e - b) / 2;
+    if (comparator(array[mid].first, key)) {
+      b = mid + 1;
+    } else {
+      e = mid;
+    }
+  }
+  return b;
 }
 
 /*
@@ -51,6 +69,8 @@ INDEX_TEMPLATE_ARGUMENTS
 KeyType B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const {
   // replace with your own code
   KeyType key;
+  assert(index >= 0 && index <= GetSize());
+  key = array[index].first;
   return key;
 }
 
@@ -61,7 +81,8 @@ KeyType B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const {
 INDEX_TEMPLATE_ARGUMENTS
 const MappingType &B_PLUS_TREE_LEAF_PAGE_TYPE::GetItem(int index) {
   // replace with your own code
-  return array[0];
+  assert(index >= 0 && index <= GetSize());
+  return array[index];
 }
 
 /*****************************************************************************
@@ -71,11 +92,21 @@ const MappingType &B_PLUS_TREE_LEAF_PAGE_TYPE::GetItem(int index) {
  * Insert key & value pair into leaf page ordered by key
  * @return  page size after insertion
  */
+
+//where is reconstruction code?
 INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::Insert(const KeyType &key,
                                        const ValueType &value,
                                        const KeyComparator &comparator) {
-  return 0;
+  auto idx = KeyIndex(key, comparator);
+  assert(idx >= 0 && idx <= GetSize() + 1);
+  assert(GetSize() < GetMaxSize());//implement this first
+  for (int i = GetSize(); i > idx; i--) {
+    array[i] = array[i - 1];
+  }
+  array[idx] = MappingType(key, value);
+  IncreaseSize(1);
+  return GetSize();
 }
 
 /*****************************************************************************
@@ -87,10 +118,21 @@ int B_PLUS_TREE_LEAF_PAGE_TYPE::Insert(const KeyType &key,
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveHalfTo(
     BPlusTreeLeafPage *recipient,
-    __attribute__((unused)) BufferPoolManager *buffer_pool_manager) {}
+    __attribute__((unused)) BufferPoolManager *buffer_pool_manager) {
+  assert(recipient != nullptr);
+  assert(GetSize() == GetMaxSize());
+  recipient->next_page_id_ = next_page_id_;
+  int length = GetMaxSize();
+  for (int i = (length + 1) / 2, j = 0; i < length; i++, j++) {
+    recipient->array[j] = array[i];
+  }
+  recipient->IncreaseSize(length - (length + 1) / 2);
+}
 
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyHalfFrom(MappingType *items, int size) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyHalfFrom(MappingType *items, int size) {
+
+}
 
 /*****************************************************************************
  * LOOKUP
@@ -103,6 +145,11 @@ void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyHalfFrom(MappingType *items, int size) {}
 INDEX_TEMPLATE_ARGUMENTS
 bool B_PLUS_TREE_LEAF_PAGE_TYPE::Lookup(const KeyType &key, ValueType &value,
                                         const KeyComparator &comparator) const {
+  auto index = KeyIndex(key, comparator);
+  if (index >= 0 && index <= GetSize()) {
+    array[index] = value;
+    return true;
+  }
   return false;
 }
 
@@ -118,7 +165,16 @@ bool B_PLUS_TREE_LEAF_PAGE_TYPE::Lookup(const KeyType &key, ValueType &value,
 INDEX_TEMPLATE_ARGUMENTS
 int B_PLUS_TREE_LEAF_PAGE_TYPE::RemoveAndDeleteRecord(
     const KeyType &key, const KeyComparator &comparator) {
-  return 0;
+  auto index = KeyIndex(key, comparator);
+  if (index >= 0 && index <= GetSize()) {
+    for (int i = index; i + 1 < GetSize(); i++) {
+      array[i] = array[i + 1];
+    }
+
+    //if(Getsize() < threshold do sth
+    return true;
+  }
+  return false;
 }
 
 /*****************************************************************************
@@ -130,9 +186,13 @@ int B_PLUS_TREE_LEAF_PAGE_TYPE::RemoveAndDeleteRecord(
  */
 INDEX_TEMPLATE_ARGUMENTS
 void B_PLUS_TREE_LEAF_PAGE_TYPE::MoveAllTo(BPlusTreeLeafPage *recipient,
-                                           int, BufferPoolManager *) {}
+                                           int, BufferPoolManager *) {
+
+}
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyAllFrom(MappingType *items, int size) {}
+void B_PLUS_TREE_LEAF_PAGE_TYPE::CopyAllFrom(MappingType *items, int size) {
+
+}
 
 /*****************************************************************************
  * REDISTRIBUTE
@@ -194,14 +254,19 @@ std::string B_PLUS_TREE_LEAF_PAGE_TYPE::ToString(bool verbose) const {
   return stream.str();
 }
 
-template class BPlusTreeLeafPage<GenericKey<4>, RID,
-                                       GenericComparator<4>>;
-template class BPlusTreeLeafPage<GenericKey<8>, RID,
-                                       GenericComparator<8>>;
-template class BPlusTreeLeafPage<GenericKey<16>, RID,
-                                       GenericComparator<16>>;
-template class BPlusTreeLeafPage<GenericKey<32>, RID,
-                                       GenericComparator<32>>;
-template class BPlusTreeLeafPage<GenericKey<64>, RID,
-                                       GenericComparator<64>>;
+template
+class BPlusTreeLeafPage<GenericKey<4>, RID,
+                        GenericComparator<4>>;
+template
+class BPlusTreeLeafPage<GenericKey<8>, RID,
+                        GenericComparator<8>>;
+template
+class BPlusTreeLeafPage<GenericKey<16>, RID,
+                        GenericComparator<16>>;
+template
+class BPlusTreeLeafPage<GenericKey<32>, RID,
+                        GenericComparator<32>>;
+template
+class BPlusTreeLeafPage<GenericKey<64>, RID,
+                        GenericComparator<64>>;
 } // namespace cmudb
