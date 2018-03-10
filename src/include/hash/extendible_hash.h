35a53d5d83ade9dd0d1ab694cbf5df8fca0454b3
Justin <justinyang339@yahoo.com>
2018-03-10 12:29:02 +0800

    added the private variables and methods in extendible_hash
    added intialization of extendible_hash constructor

--- src/include/hash/extendible_hash.h
+++ src/include/hash/extendible_hash.h
@@ -17,23 +17,50 @@
 
 namespace cmudb {
 
-template <typename K, typename V>
-class ExtendibleHash : public HashTable<K, V> {
-public:
-  // constructor
-  ExtendibleHash(size_t size);
-  // helper function to generate hash addressing
-  size_t HashKey(const K &key);
-  // helper function to get global & local depth
-  int GetGlobalDepth() const;
-  int GetLocalDepth(int bucket_id) const;
-  int GetNumBuckets() const;
-  // lookup and modifier
-  bool Find(const K &key, V &value) override;
-  bool Remove(const K &key) override;
-  void Insert(const K &key, const V &value) override;
+    template<typename K, typename V>
+    class ExtendibleHash : public HashTable<K, V> {
+    public:
+        // constructor
+        ExtendibleHash(size_t size);
 
-private:
-  // add your own member variables here
-};
+        // helper function to generate hash addressing
+        size_t HashKey(const K &key);
+
+        // helper function to get global & local depth
+        int GetGlobalDepth() const;
+
+        int GetLocalDepth(int bucket_id) const;
+
+        int GetNumBuckets() const;
+
+        // lookup and modifier
+        bool Find(const K &key, V &value) override;
+
+        bool Remove(const K &key) override;
+
+        void Insert(const K &key, const V &value) override;
+
+    private:
+        // the struct to hold an entry in the bucket
+        struct entry{
+            //todo: find out the type of key and value
+            //this hash table uses a template class --> generic programming?
+        };
+
+        // the struct to hold the bucket object
+        struct bucket{
+            size_t local_bits; // local # of bits use for selecting index
+            entry *entries; // pointer to the array of entries in a bucket
+
+        };
+
+
+        size_t bucket_num_; // number of buckets in the hash table
+        size_t array_size_; // fixed array size for each bucket
+        size_t global_bits_; // global # of bits used for selecting index
+        std::vector<bucket*> *buckets; //the vector to hold the array of buckets
+        const int DEFAULT_BUCKET_NUM = 4; // default bucket number
+        const int DEFAULT_ENTRY_NUM = 8; // default <k,v> entry in a bucket
+
+    };
 } // namespace cmudb
